102b4b543a6b7c0a2192e2620565d6e4df4907ad
Anwesh43 <anweshthecool0@gmail.com>
2018-02-17 08:30:55 +0530

    adding listener in Activity

--- app/src/main/java/ui/anwesome/com/kotlinsquarejoinerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsquarejoinerview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlinsquarejoinerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.squarejoinerview.SquareJoinerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SquareJoinerView.create(this)
+        val view = SquareJoinerView.create(this)
+        view.addOnSquareJoinListener {
+            Toast.makeText(this, "square joined",Toast.LENGTH_SHORT).show()
+        }
     }
 }
